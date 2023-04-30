a4a69d6e58895291c1539890ca6a4657621a99b7
Albert Wu <wua@uber.com>
2023-04-29 23:41:26 -0700

    update stats

--- stats.py
+++ stats.py
@@ -1,49 +1,116 @@
+import os
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
+from textblob import TextBlob
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from collections import Counter
+import nltk
 
-# Read the CSV file
-df = pd.read_csv('all_channel_conversations.csv')
+nltk.download('punkt')
+nltk.download('stopwords')
 
-# Convert timestamp to datetime objects
-df['timestamp'] = pd.to_datetime(df['timestamp'])
+def read_csv_data(file_name):
+    messages_df = pd.read_csv(file_name, parse_dates=['timestamp'])
+    return messages_df
 
-# Calculate the stats
-messages_per_user_month = df.groupby([df['username'], df['timestamp'].dt.to_period('M')]).size().unstack().fillna(0)
-average_message_length_per_user_month = df.groupby([df['username'], df['timestamp'].dt.to_period('M')])['message'].apply(lambda x: x.str.len().mean()).unstack().fillna(0)
-average_messages_per_day_month = messages_per_user_month.div(messages_per_user_month.columns.to_timestamp().days_in_month, axis=1)
+def most_active_users_chart(messages_df):
+    user_message_counts = messages_df['username'].value_counts()
+    user_message_counts = user_message_counts[user_message_counts >= 5]
+    return user_message_counts
 
-# Remove users with less than 1 message in each month
-messages_per_user_month = messages_per_user_month[messages_per_user_month.gt(0).all(axis=1)]
-average_message_length_per_user_month = average_message_length_per_user_month.loc[messages_per_user_month.index]
-average_messages_per_day_month = average_messages_per_day_month.loc[messages_per_user_month.index]
+def most_active_channels_chart(messages_df):
+    channel_message_counts = messages_df['channel'].value_counts()
+    channel_message_counts = channel_message_counts[channel_message_counts >= 10]
+    return channel_message_counts
 
-# Create a 3-row, 1-column grid of plots
-fig, ax = plt.subplots(3, 1, figsize=(10, 15), gridspec_kw={'height_ratios': [3, 3, 4]})
-fig.subplots_adjust(hspace=0.5)  # Add extra padding between subplots
 
-# Plot number of messages per user per month
-messages_per_user_month.T.plot(ax=ax[0], kind='bar', stacked=True)
-ax[0].set_title('Number of Messages per User per Month')
-ax[0].set_xlabel('Month')
-ax[0].set_ylabel('Number of Messages')
-ax[0].legend(loc='upper left', bbox_to_anchor=(1, 1))
+def peak_activity_times_chart(messages_df):
+    messages_df['hour'] = messages_df['timestamp'].dt.hour
+    hourly_message_counts = messages_df.groupby('hour')['message'].count()
+    return hourly_message_counts
 
-# Plot average message length per user per month
-average_message_length_per_user_month.T.plot(ax=ax[1], kind='bar')
-ax[1].set_title('Average Message Length per User per Month')
-ax[1].set_xlabel('Month')
-ax[1].set_ylabel('Average Message Length')
-ax[1].legend(loc='upper left', bbox_to_anchor=(1, 1))
+def message_sentiment_chart(messages_df, filtered_channels):
+    messages_df = messages_df[messages_df['channel'].isin(filtered_channels)].copy()
+    messages_df['message'] = messages_df['message'].astype(str)
+    messages_df['sentiment'] = messages_df['message'].apply(lambda text: TextBlob(text).sentiment.polarity)
+    channel_sentiments = messages_df.groupby('channel')['sentiment'].mean().sort_values()
+    return channel_sentiments
 
-# Plot average messages per day for each user per month (line chart)
-average_messages_per_day_month.T.plot(ax=ax[2])
-ax[2].set_title('Average Messages per Day for Each User per Month (Line Chart)')
-ax[2].set_xlabel('Month')
-ax[2].set_ylabel('Average Number of Messages per Day')
-ax[2].legend(loc='upper left', bbox_to_anchor=(1, 1))
 
-# Save the plots as a PDF
-with PdfPages('user_stats_monthly.pdf') as pdf:
-    pdf.savefig(fig, bbox_inches='tight')
+def top_keywords_chart(messages_df):
+    stop_words = set(stopwords.words('english'))
+    messages_df['message'] = messages_df['message'].astype(str)
+    all_messages = ' '.join(messages_df['message'].tolist())
+    words = word_tokenize(all_messages)
+    filtered_words = [word for word in words if word.isalnum() and word.lower() not in stop_words]
+    word_frequencies = Counter(filtered_words).most_common(20)
+    return word_frequencies
 
+
+def generate_charts(csv_file, output_file):
+    messages_df = read_csv_data(csv_file)
+
+    # Generate charts
+    user_message_counts = most_active_users_chart(messages_df)
+    channel_message_counts = most_active_channels_chart(messages_df)
+    hourly_message_counts = peak_activity_times_chart(messages_df)
+    filtered_channels = channel_message_counts.index
+    channel_sentiments = message_sentiment_chart(messages_df, filtered_channels)
+    word_frequencies = top_keywords_chart(messages_df)
+
+    # Plot the charts
+    with PdfPages(output_file) as pdf:
+        fig, ax = plt.subplots()
+        fig.subplots_adjust(bottom=0.2)  # Add room at bottom
+        user_message_counts.plot(kind='bar', ax=ax)
+        ax.set_title('Most Active Users')
+        ax.set_xlabel('User')
+        ax.set_ylabel('Number of Messages')
+        pdf.savefig()
+        plt.close()
+
+        fig, ax = plt.subplots()
+        fig.subplots_adjust(bottom=0.3)  # Add room at bottom
+        channel_message_counts.plot(kind='bar', ax=ax)
+        ax.set_title('Most Active Channels')
+        ax.set_xlabel('Channel')
+        ax.set_ylabel('Number of Messages')
+        pdf.savefig()
+        plt.close()
+
+        fig, ax = plt.subplots()
+        hourly_message_counts.plot(kind='bar', ax=ax)
+        ax.set_title('Peak Activity Times')
+        ax.set_xlabel('Hour of Day')
+        ax.set_ylabel('Number of Messages')
+        pdf.savefig()
+        plt.close()
+
+        # Plot the Message Sentiment chart
+        fig, ax = plt.subplots()
+        fig.subplots_adjust(bottom=0.3)  # Add room at bottom
+        channel_sentiments.plot(kind='bar', ax=ax)
+        ax.set_title('Message Sentiment by Channel')
+        ax.set_xlabel('Channel')
+        ax.set_ylabel('Sentiment Score')
+        pdf.savefig()
+        plt.close()
+
+        fig, ax = plt.subplots()
+        fig.subplots_adjust(bottom=0.2)  # Add room at bottom
+        word_freq_df = pd.DataFrame(word_frequencies, columns=['Word', 'Frequency'])
+        word_freq_df.plot(kind='bar', x='Word', y='Frequency', ax=ax)
+        ax.set_title('Top Keywords')
+        ax.set_xlabel('Keyword')
+        ax.set_ylabel('Frequency')
+        pdf.savefig()
+        plt.close()
+
+
+if __name__ == '__main__':
+    csv_file = 'all_channel_conversations.csv'
+    output_file = 'slack_stats_charts.pdf'
+    generate_charts(csv_file, output_file)
